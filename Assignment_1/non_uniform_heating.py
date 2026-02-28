955422763cf0224149cdd40dedf487b36f757c3b
Divyam <divyampandeyy@outlook.com>
2026-02-28 17:12:35 +0530

    Initialized Assignment 1

---
+++ Assignment_1/non_uniform_heating.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameters
+l = 1.0  # Length of the plate
+w = 1.0  # Width of the plate
+nx_elems = 10  # Number of elements in x direction
+ny_elems = 10  # Number of elements in y direction
